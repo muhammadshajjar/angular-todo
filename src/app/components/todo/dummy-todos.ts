32b724f34069de750ecbf4cb7a23adb9932a379b
Muhammad Shajjar <muhammadshajjar99@gmail.com>
2024-07-23 09:54:03 +0500

    transform todo date using pipe

--- src/app/components/todo/dummy-todos.ts
+++ src/app/components/todo/dummy-todos.ts
@@ -2,11 +2,11 @@ export const TODOS = [
   {
     id: '1',
     title: 'Learn routes in angular',
-    date_added: '23/07/2024',
+    date_added: '7/22/2024',
   },
   {
     id: '2',
     title: 'Learn directives in angular',
-    date_added: '22/07/2024',
+    date_added: '7/20/2024',
   },
 ];
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Todo } from './todo.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css',
 })
